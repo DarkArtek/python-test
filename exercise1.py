da90966733bf2defae58c930755c243d631ec24f
DarkArtek <darkartek@gmail.com>
2014-03-11 10:51:35 +0100

    Updated exercise 1

--- exercise1.py
+++ exercise1.py
@@ -1,9 +1,10 @@
-from collections import OrderedDict
+def dict_values_ascending(things):
+	res = list(sorted(things, key=words.__getitem__))
+	print res
+	dict = {
+	'marco': 'desk 1',
+	'giulio': 'desk 2',
+	'mike': 'room 3'
+	}
 
-d = { 'marco': 'desk 1', 'giulio': 'desk 2', 'mike': 'desk 3' }
-
-
-OrderedDict(sorted(d.items(), key=lambda t: t[desk1]))
-OrderedDict([('mike',desk3), ('giulio', 2), ('marco', 1)])
-
-#Non so come continuare, mi spiace.
+	dict_values_ascending(dict)
