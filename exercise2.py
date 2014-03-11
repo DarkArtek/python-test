73277c00e1da7d03444981a7877e2c7052123f50
DarkArtek <darkartek@gmail.com>
2014-03-11 10:57:32 +0100

    Updated exercises

---
+++ exercise2.py
@@ -0,0 +1,14 @@
+def add_dict(dict_1, dict_2):
+	res = list.append(dict_2)
+	print res
+	dict_1 = {
+			'pippo': 'a',
+			'pluto': 'b',
+			'paperino': 'c'
+	}
+
+	dict_2 = {
+			'qui': 'd',
+			'quo': 'e',
+			'qua': 'f'
+	}
