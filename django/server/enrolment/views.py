bc4ac646f0055ade2d42db66a5ab0ad8a8391c85
DarkArtek <darkartek@gmail.com>
2014-03-11 12:15:35 +0100

    Updated Django Prject

--- django/server/enrolment/views.py
+++ django/server/enrolment/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from enrloment.models import EnrModel
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 
 # Create your views here.
