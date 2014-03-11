03e68ad13f79199ebab41187c985b7f4e66fab6b
DarkArtek <darkartek@gmail.com>
2014-03-11 11:01:20 +0100

    Updated Structure

---
+++ language_exercises/exercise1.py
@@ -0,0 +1,10 @@
+def dict_values_ascending(things):
+	res = list(sorted(things, key=words.__getitem__))
+	print res
+	dict = {
+	'marco': 'desk 1',
+	'giulio': 'desk 2',
+	'mike': 'room 3'
+	}
+
+	dict_values_ascending(dict)
---
+++ language_exercises/exercise2.py
@@ -0,0 +1,14 @@
+def add_dict(bands, countries):
+	res = list.append(countries)
+	print res
+	bands = {
+			'IronMaiden': '1',
+			'BlackSabbath': '2',
+			'ACDC': '3'
+	}
+
+	countries = {
+			'UK': '4',
+			'USA': '5',
+			'UK': '6'
+	}
