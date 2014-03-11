90f196cc2c1001bb6624eb0be3468a6eba1ebf8a
DarkArtek <darkartek@gmail.com>
2014-03-11 10:48:36 +0100

    Few Exercises done

---
+++ exercise1.py
@@ -0,0 +1,9 @@
+from collections import OrderedDict
+
+d = { 'marco': 'desk 1', 'giulio': 'desk 2', 'mike': 'desk 3' }
+
+
+OrderedDict(sorted(d.items(), key=lambda t: t[desk1]))
+OrderedDict([('mike',desk3), ('giulio', 2), ('marco', 1)])
+
+#Non so come continuare, mi spiace.
---
+++ exercise4.py
@@ -0,0 +1,5 @@
+first = raw_input("Insert first word: ")
+second = raw_input("Insert Second word: ")
+
+print first
+print second
